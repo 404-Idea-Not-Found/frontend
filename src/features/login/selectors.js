86943fe6900111a1f477378678efcd1b971eee62
DanK <ddad9583@gmail.com>
2022-03-09 18:22:46 +0900

    [hotfix] fix_first_meeting_video_not_loading

--- src/features/login/selectors.js
+++ src/features/login/selectors.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 export const selectLoginError = (state) => state.login.error;
 export const selectIsLoggedIn = (state) => state.login.isLoggedIn;
 export const selectUserId = (state) => state.login.userId;
