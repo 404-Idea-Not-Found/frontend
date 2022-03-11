f95356de0cb75366d69954c1e735a661342db37f
DanK <ddad9583@gmail.com>
2022-03-12 00:55:50 +0900

    [hotfix] add missing file

---
+++ src/features/myPage/MyPage.js
@@ -0,0 +1,105 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+
+import ErrorMessage from "../../common/components/ErrorMessage";
+import Header from "../../common/components/Header";
+import Loader from "../../common/components/Loader";
+import { COLOR } from "../../common/util/constants";
+import { selectUserEmail, selectUserId } from "../login/selectors";
+import MyMeetingCardList from "./MyMeetingCardList";
+import { createGetMyPageMeetingAction } from "./myPageSagas";
+import { resetMyPageSlice } from "./myPageSlice";
+import {
+  selectError,
+  selectIsApiLoading,
+  selectIsLoading,
+  selectMeetingList,
+} from "./selectors";
+
+const MyPageContainer = styled.div`
+  min-width: 1300px;
+  height: 100vh;
+
+  & ::-webkit-scrollbar {
+    display: block;
+  }
+
+  & ::-webkit-scrollbar {
+    width: 0.5rem;
+  }
+
+  & ::-webkit-scrollbar-thumb {
+    background: ${COLOR.GREY};
+  }
+
+  & ::-webkit-scrollbar-thumb:hover {
+    background: black;
+  }
+
+  & ::-webkit-scrollbar-track-piece {
+    background-color: ${COLOR.LIGHT_GREY};
+  }
+
+  .meeting-list-wrapper {
+    width: 100%;
+    margin: 0 auto;
+    height: calc(100% - 1rem - 22px);
+    overflow-y: scroll;
+  }
+`;
+
+function MyPage() {
+  const userId = useSelector(selectUserId);
+  const email = useSelector(selectUserEmail);
+  const meetingList = useSelector(selectMeetingList);
+  const error = useSelector(selectError);
+  const isLoading = useSelector(selectIsLoading);
+  const isApiLoading = useSelector(selectIsApiLoading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (userId && email) dispatch(createGetMyPageMeetingAction(userId, email));
+  }, [userId, email, dispatch]);
+
+  useEffect(
+    () => () => {
+      dispatch(resetMyPageSlice());
+    },
+    []
+  );
+
+  return (
+    <MyPageContainer>
+      <Header />
+      {(!userId || !email) && (
+        <ErrorMessage errorMessage="로그인이 필요 합니다!" />
+      )}
+      {isLoading && <Loader spinnerWidth="300px" containerHeight="100%" />}
+      {isApiLoading && <Loader spinnerWidth="300px" containerHeight="100%" />}
+      {error.isError && <ErrorMessage errorMessage={error.errorMessage} />}
+      {!isApiLoading && !isLoading && !error.isError && (
+        <div className="meeting-list-wrapper">
+          <MyMeetingCardList
+            meetingType="진행 예정 미팅"
+            meetingList={meetingList.plannedMeeting}
+          />
+          <MyMeetingCardList
+            meetingType="지난 미팅"
+            meetingList={meetingList.pastMeeting}
+          />
+          <MyMeetingCardList
+            meetingType="예약한 미팅"
+            meetingList={meetingList.reservedMeeting}
+          />
+          <MyMeetingCardList
+            meetingType="참여중인 프로젝트"
+            meetingList={meetingList.participatingProject}
+          />
+        </div>
+      )}
+    </MyPageContainer>
+  );
+}
+
+export default MyPage;
---
+++ src/features/myPage/myPageSagas.js
@@ -0,0 +1,89 @@
+import { call, put, takeEvery } from "redux-saga/effects";
+
+import cancelMeetingReservation from "../../common/api/cancelMeetingReservation";
+import deleteMeeting from "../../common/api/deleteMeeting";
+import fetchMyPageMeetingList from "../../common/api/fetchMyPageMeetingList";
+import getErrorMessage from "../../common/util/getErrorMessage";
+import sleep from "../../common/util/sleep";
+import {
+  meetingListLoaded,
+  myPageErrorHappened,
+  meetingDeleted,
+  calledApi,
+  apiLoaded,
+  reservationCanceled,
+} from "./myPageSlice";
+
+const myPageSagaActionCreators = {
+  createGetMyPageMeetingAction: (userId, email) => ({
+    type: "GET_MY_PAGE_MEETING",
+    payload: { userId, email },
+  }),
+  createCancelMeetingAction: (meetingId) => ({
+    type: "CANCEL_MEETING",
+    payload: { meetingId },
+  }),
+  createCancelReservationAction: (meetingId) => ({
+    type: "CANCEL_RESERVATION",
+    payload: { meetingId },
+  }),
+};
+
+function* getMyPageMeeting(action) {
+  try {
+    const { data } = yield call(
+      fetchMyPageMeetingList,
+      action.payload.userId,
+      action.payload.email
+    );
+
+    yield put(meetingListLoaded(data.catagorizedMyPageMeeting));
+  } catch (error) {
+    const errorMessage = getErrorMessage(error);
+    yield put(myPageErrorHappened(errorMessage));
+  }
+}
+
+function* cancelMeeting(action) {
+  try {
+    yield put(calledApi());
+    yield call(deleteMeeting, action.payload.meetingId);
+    yield put(meetingDeleted(action.payload.meetingId));
+    yield call(sleep, 500);
+    yield put(apiLoaded());
+  } catch (error) {
+    const errorMessage = getErrorMessage(error);
+    yield put(myPageErrorHappened(errorMessage));
+  }
+}
+
+function* cancelReservation(action) {
+  try {
+    yield put(calledApi());
+    yield call(cancelMeetingReservation, action.payload.meetingId);
+    yield put(reservationCanceled(action.payload.meetingId));
+    yield call(sleep, 500);
+    yield put(apiLoaded());
+  } catch (error) {
+    const errorMessage = getErrorMessage(error);
+    yield put(myPageErrorHappened(errorMessage));
+  }
+}
+
+export function* watchGetMyPageMeeting() {
+  yield takeEvery("GET_MY_PAGE_MEETING", getMyPageMeeting);
+}
+
+export function* watchCancelMeeting() {
+  yield takeEvery("CANCEL_MEETING", cancelMeeting);
+}
+
+export function* watchCancelReservation() {
+  yield takeEvery("CANCEL_RESERVATION", cancelReservation);
+}
+
+export const {
+  createGetMyPageMeetingAction,
+  createCancelMeetingAction,
+  createCancelReservationAction,
+} = myPageSagaActionCreators;
